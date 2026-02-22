feb5f8d8458e9b599bdb09b0e7f0eafc1a6c4407
Mateus Abreu <abreufagundesmateus@gmail.com>
2026-02-22 20:50:11 -0300

    Adição de rota no controller do documento instrutor

--- demo/src/main/java/com/api/demo/controller/DocumentoInstrutorController.java
+++ demo/src/main/java/com/api/demo/controller/DocumentoInstrutorController.java
@@ -4,20 +4,18 @@ import com.api.demo.model.DocumentoInstrutor;
 import com.api.demo.services.DocumentoInstrutorServices;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 
 @Controller
 @RequestMapping("/documentos-instrutores")
 public class DocumentoInstrutorController {
     private final DocumentoInstrutorServices documentoInstrutorServices;
 
-    public DocumentoInstrutorController(DocumentoInstrutorServices documentoInstrutorServices){
+    public DocumentoInstrutorController(DocumentoInstrutorServices documentoInstrutorServices) {
         this.documentoInstrutorServices = documentoInstrutorServices;
     }
 
@@ -34,5 +32,14 @@ public class DocumentoInstrutorController {
 
         return ResponseEntity.ok(documentoInstrutor);
     }
-}
 
+    //id do instrutor
+    @GetMapping("/instrutores/{id}")
+    public ResponseEntity<List<DocumentoInstrutor>> findByInstrutor(
+            @PathVariable Long id
+    ) {
+        List<DocumentoInstrutor> documentoInstrutorList = documentoInstrutorServices.findByInstrutor(id);
+
+        return ResponseEntity.ok(documentoInstrutorList);
+    }
+}
